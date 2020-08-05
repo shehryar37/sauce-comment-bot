953231322df2de1ccca10389e4e503037fdb407f
shehryar37 <sasubhani7@outlook.com>
2020-08-05 19:34:26 +0500

    Reverted default comment

--- data.py
+++ data.py
@@ -8,13 +8,14 @@ subreddit_list = ['animemes', 'manga', 'anime_irl',
 sauce_request = r"(w+h+a*t+ *)+('*i*s+ *)*(t+h+e+ *)*(s+a+u+c+e+s*)|(s+a+u+c+e+s*\?+)|^(s+a+u+c+e+s*)$"
 
 # sauce checker
-sauce_regex = r"([\{]+[A-Za-z-:/!? ]+[\}]+)|([\]]+[A-Za-z-:/!? ]+[\[]+)|([\<]+[A-Za-z-:/!? ]+[\>]+)|([\|]+[A-Za-z-:/!? ]+[\|]+)|([\(]+[0-9]{1,7}[\)]+)"
+sauce_regex = r"([\{]+[A-Za-z0-9-:/!? ]+[\}]+)|([\]]+[A-Za-z0-9-:/!? ]+[\[]+)|([\<]+[A-Za-z0-9-:/!? ]+[\>]+)|([\|]+[A-Za-z0-9-:/!? ]+[\|]+)|([\(]+[0-9]{1,7}[\)]+)"
 sauce_keyword = r"!sauce"
 
 bots = ['Roboragi', 'nHentaiTagBot']
 
 default_replies = [
-    '''Konnichiwa! No one has provided the sauce using bots so far. If you are seeing this message and know the sauce, please reply with the sauce enveloped inside {}, <>, ||, (), or ][ ([a detailed explanation](https://www.reddit.com/r/Roboragi/wiki/index#wiki_how_do_i_use_it.3F)) so that I can automatically provide the sauce to any user who asks for it in the future.''',
+    '''Konnichiwa! No one has provided the sauce using bots so far.
+    If you are seeing this message and know the sauce, please reply with the sauce enveloped inside {}, <>, ||, (), or ][ ([a detailed explanation](https://www.reddit.com/r/Roboragi/wiki/index#wiki_how_do_i_use_it.3F)) so that I can automatically provide the sauce to any user who asks for it in the future.''',
     '''Ohiyo!! No one has provided the sauce using bots so far.
     If you are seeing this message and know the sauce, please reply with the sauce enveloped inside {}, <>, ||, (), or ][ ([a detailed explanation.](https://www.reddit.com/r/Roboragi/wiki/index#wiki_how_do_i_use_it.3F)) But it's not like I want you to give sauce or anything ba-BAKA!! >.<''',
     '''Yare yare daze, no one has provided the sauce using bots so far.
