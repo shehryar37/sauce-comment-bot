3af388bfad8bf94e2816f601095a17cabd5cbefe
shehryar37 <sasubhani7@outlook.com>
2020-08-06 17:33:12 +0500

    Regex update + default reply changes

--- data.py
+++ data.py
@@ -5,10 +5,10 @@ subreddit_list = ['animemes', 'manga',
 # Add hentai after August 11
 
 # keyphrase
-sauce_request = r"(w+h+a*t+ *)+('*i*s+ *)*(t+h+e+ *)*(s+a+u+c+e+s*)|(s+a+u+c+e+s*\?+)|^(s+a+u+c+e+s*)$"
+sauce_request = r"[(w+h+a*t+ *)(w+h+e+r+e *)]+('*i*s+ *)*(t+h+e+ *)*(s+a+u+c+e+s*)|(s+a+u+c+e+s*\?+)|^(s+a+u+c+e+s*)$"
 
 # sauce provided by OP checker
-sauce_provided_keyword = r"(!sauce)|(sauce:)"
+sauce_provided_keyword = r"[(t+h+i+s+ *)(h+e+r+e+ *)]+('*i*s+ *)*(t+h+e+ *)*(s+a+u+c+e+)|(!+s+a+u+c+e+)|(s+a+u+c+e+:)"
 
 # sauce request checker
 sauce_regex = r"([\{]+[A-Za-z0-9-:/!? ]+[\}]+)|([\]]+[A-Za-z0-9-:/!? ]+[\[]+)|([\<]+[A-Za-z0-9-:/!? ]+[\>]+)|([\|]+[A-Za-z0-9-:/!? ]+[\|]+)|([\(]+[0-9]{1,7}[\)]+)"
@@ -19,16 +19,19 @@ bots = ['Roboragi', 'nHentaiTagBot']
 sauce_reply = "Thank you for providing sauce! I will ensure that it is passed on to anyone who asks for it in the comments later on. This subreddit needs more people like you! Arigato!!"
 
 default_replies = [
-    '''Konnichiwa! No one has provided thde sauce using bots so far.    \
+    '''Konnichiwa! No one has provided the sauce using bots so far.
 
 
-    ^(If you are seeing this message and know the sauce, please reply with the sauce enveloped inside) ^{}, ^<>, ^||, ^(\(\))^(, or )^][ ^(\()[^(a detailed explanation)](https://www.reddit.com/r/Roboragi/wiki/index#wiki\_how\_do\_i\_use\_it.3F)^(\)) ^(so that I can automatically provide the sauce to any user who asks for it in the future.)''',
-    '''Ohiyo!! No one has provided the sauce using bots so far.    \
+___
+^(If you are seeing this message and know the sauce, please reply with the sauce enveloped inside) ^{}, ^<>, ^||, ^(\(\))^(, or )^][ ^(\()[^(a detailed explanation)](https://www.reddit.com/r/Roboragi/wiki/index#wiki\_how\_do\_i\_use\_it.3F)^(\)) ^(so that I can automatically provide the sauce to any user who asks for it in the future.)''',
+    '''Ohiyo!! No one has provided the sauce using bots so far.
 
 
-    ^(If you are seeing this message and know the sauce, please reply with the sauce enveloped inside) ^{}, ^<>, ^||, ^(\(\))^(, or )^][ ^(\()[^(a detailed explanation)](https://www.reddit.com/r/Roboragi/wiki/index#wiki\_how\_do\_i\_use\_it.3F)^(\)) ^(But it's not like I want you to give sauce or anything ba-BAKA!! >.<)''',
-    '''Yare yare daze, no one has provided the sauce using bots so far.    \
+___
+^(If you are seeing this message and know the sauce, please reply with the sauce enveloped inside) ^{}, ^<>, ^||, ^(\(\))^(, or )^][ ^(\()[^(a detailed explanation)](https://www.reddit.com/r/Roboragi/wiki/index#wiki\_how\_do\_i\_use\_it.3F)^(\)) ^(But it's not like I want you to give sauce or anything ba-BAKA!! >.<)''',
+    '''Yare yare daze, no one has provided the sauce using bots so far.
 
 
-    ^(If you are seeing this message and know the sauce, please reply with the sauce enveloped inside) ^{}, ^<>, ^||, ^(\(\))^(, or )^][ ^(\()[^(a detailed explanation)](https://www.reddit.com/r/Roboragi/wiki/index#wiki\_how\_do\_i\_use\_it.3F)^(\)) ^(so that I can give the damn sauce to any annoying bitch that asks for it in the future)'''
+___
+^(If you are seeing this message and know the sauce, please reply with the sauce enveloped inside) ^{}, ^<>, ^||, ^(\(\))^(, or )^][ ^(\()[^(a detailed explanation)](https://www.reddit.com/r/Roboragi/wiki/index#wiki\_how\_do\_i\_use\_it.3F)^(\)) ^(so that I can give the damn sauce to any annoying bitch that asks for it in the future)'''
 ]
